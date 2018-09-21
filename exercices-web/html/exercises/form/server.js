e51025561fa7483017bdd721d73a2a6f89fbfba7
victorcabeceira <victorfgcabeceira@gmail.com>
2018-09-21 19:18:24 -0300

    Forms #2

--- exercices-web/html/exercises/form/server.js
+++ exercices-web/html/exercises/form/server.js
@@ -6,7 +6,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post('/users', (req, resp) => {
     console.log(req.body)
-    resp.send('<h1>Included!</h1>')
+    resp.send('<h1>Signed up!!</h1>')
+})
+
+app.post('/users/:id', (req, resp) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('<h1>Congratulations, user saved!!</h1>')
 })
 
 app.listen(3003)
