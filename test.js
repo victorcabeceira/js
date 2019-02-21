e672e5c056a40bdf4b8b0a693567cc8946c31e64
victor <victorfgcabeceira@gmail.com>
2019-02-21 13:54:46 -0300

    Adding gitignore

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const instance = axios.create({
   baseURL: 'https://swapi.co/api/'
 })
 
-getData = () => {
+getData = async () => {
   return new Promise((res, rej) => {
     instance.get('people/')
       .then(result => res(result.data))
@@ -21,3 +21,20 @@ getData().then(data => {
   console.log('orderedArrayLodash', orderedArrayLodash)
   console.log('orderedArraySort', orderedArraySort)
 })
+
+const logger = async () => {
+  try {
+    const result = await getData();
+    const unorderedArray = results.map(d => ({ name: d.name, height: d.height }));
+    const orderedArrayLodash = _.sortBy(unorderedArray, ['name', 'age']);
+    const orderedArraySort = unorderedArray.sort((a,b) => { return a.name > b.name });
+    console.log('result 2222222', result);
+    console.log('unorderedArray', unorderedArray)
+    console.log('orderedArrayLodash', orderedArrayLodash)
+    console.log('orderedArraySort', orderedArraySort)
+  } catch (error) {
+    console.log('error', error);
+  }
+}
+
+logger()
